45d6836be4df289182624e4bdd442cf5b372fb42
unknown <ryoken@mail.ru>
2021-04-22 11:28:21 +0300

    lesson2

--- project/js/main.js
+++ project/js/main.js
@@ -1,26 +1,62 @@
 "use strict";
 
-const products = [
-    { id: 1, title: 'Notebook', price: 20000 },
-    { id: 2, title: 'Mouse', price: 1500 },
-    { id: 3, title: 'Keyboard', price: 5000 },
-    { id: 4, title: 'Gamepad', price: 4500 },
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this._goods = []; // data
+        this._allProducts = []; // массив экземпляров товаров на основе this._goods
 
-const renderProduct = (title = "", price = 0) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
+        this._fetchGoods();
+        this._render();
+    }
 
-const renderProducts = (list = []) => {
-    //в innerHTML мы передаем строковое представление массива (метод toString() - запятая в качестве разделителя) 
-    //для преобразования массива в строку использую дополнительно join с нужным разделителем
-    return list
-        .map((item) => renderProduct(item.title, item.price))
-        .join(' ');
+    _fetchGoods() {
+        this._goods = [
+            { id: 1, title: 'Notebook', price: 20000 },
+            { id: 2, title: 'Mouse', price: 1500 },
+            { id: 3, title: 'Keyboard', price: 5000 },
+            { id: 4, title: 'Gamepad', price: 4500 },
+        ];
+    }
+
+    _render() {
+        const block = document.querySelector(this.container);
+
+        for (const product of this._goods) {
+            // console.log(new ProductItem(product).render());
+            const productObject = new ProductItem(product);
+
+            this._allProducts.push(productObject);
+            block.insertAdjacentHTML('beforeend', productObject.render());
+        }
+    }
+
+    fullPrice() {
+        const block = document.querySelector(this.container);
+        let price = this._goods.reduce((sum, { price }) => sum + price, 0); //решил посмотреть как reduce работает для интереса, можно еще через перебор в цикле
+        block.insertAdjacentHTML('afterend', `<div class="product-full-price">Полная стоимость: ${price} \u20bd</div>`);
+    }
 }
 
-document.querySelector('.products').innerHTML = renderProducts(products);
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                  <img src="${this.img}" alt="Some img">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price} \u20bd</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
+              </div>`;
+    }
+}
+
+const catalog = new ProductList();
+catalog.fullPrice();
